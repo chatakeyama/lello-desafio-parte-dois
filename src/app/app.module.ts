981df7d71dde6727ca5b1dbdd101119435b9fcbb
Camila Hatakeyama <hatakeyama.camila@gmail.com>
2021-01-30 15:45:41 -0300

    Remoção de arquivos não utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MemberListComponent } from './components/members/member-list/member-list.component';
-import { MemberComponent } from './components/members/member/member.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { ModalMemberInfoComponent } from './components/members/modal-member-info/modal-member-info.component';
 import { DarkenOnHoverDirective } from './directives/darken-on-hover.directive';
@@ -15,7 +14,6 @@ import { DarkenOnHoverDirective } from './directives/darken-on-hover.directive';
   declarations: [
     AppComponent,
     MemberListComponent,
-    MemberComponent,
     SearchBarComponent,
     ModalMemberInfoComponent,
     DarkenOnHoverDirective,
